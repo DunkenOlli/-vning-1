ccbec3d9d3ffdcd698722549e53e5bc861ea328c
qwert y <you@example.com>
2019-04-01 13:35:53 +0200

    Initial commit

---
+++ Uppgift21.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+
+public class Uppgift21 {
+
+	public static void main(String[] args) {
+		
+	String date = Calendar.getInstance().getTime().toString();	
+	System.out.println("Klockan är: " + date.substring(11, 19));
+				
+		//Hej hej, detta är ett test	
+	}
+
+}
